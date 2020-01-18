19e69d94faaeed2389f0d3e2bafca60bf0b050be
Brandon Kerns <bkerns@uw.edu>
2020-01-17 17:02:21 -0800

    Move option to top.

--- MASTER_RUN/lpt_driver.py
+++ MASTER_RUN/lpt_driver.py
@@ -102,13 +102,13 @@ lpo_options['cold_start_const_period'] = 24.0  # hours
 lpt_options={}
 #lpt_options['do_lpt_calc'] = False
 lpt_options['do_lpt_calc'] = True
+lpt_options['do_lpt_individual_masks'] = True  # Whether to generate mask files for each LPT system.
+lpt_options['do_lpt_composite_mask'] = True    # Whether to generate mask file for all LPT systems combined.
 lpt_options['min_overlap_points'] = 1600      # LP object connectivity is based on either points
 lpt_options['min_overlap_frac'] = 0.5         # -- OR fraction of either LP object.
 lpt_options['min_lp_objects_points'] = 400    # Disregard LP objects smaller than this.
 lpt_options['min_lpt_duration_hours'] = 7*24  # Minumum duration to keep it as an LPT (hours)
 lpt_options['center_jump_max_hours'] = 3*24   # How long to allow center jumps (hours)
-lpt_options['do_lpt_individual_masks'] = True  # Whether to generate mask files for each LPT system.
-lpt_options['do_lpt_composite_mask'] = True    # Whether to generate mask file for all LPT systems combined.
 
 ## Merging/Splitting settings
 merge_split_options={}
