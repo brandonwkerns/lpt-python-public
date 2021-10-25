d2e582e514aa0f31582034ddfea599a701896e78
brandonwkerns <bkerns@uw.edu>
2021-10-25 01:03:06 -0700

    BK/Update: Composite masks uses cftime and xarray.

--- lpt/masks.py
+++ lpt/masks.py
@@ -512,7 +512,7 @@ def calc_individual_lpt_masks(dt_begin, dt_end, interval_hours, prod='trmm'
 
             nnnn = int(str(int(lp_object_id))[-4:])
             try:
-                dt_this0 = lpt.helpers.get_objid_datetime(lp_object_id) # dt.datetime.strptime(str(int(lp_object_id))[0:10],'%Y%m%d%H')
+                dt_this0 = lpt.helpers.get_objid_datetime(lp_object_id)
                 dt_this = cftime.datetime(dt_this0.year,dt_this0.month,dt_this0.day,dt_this0.hour,calendar=TC['datetime'][0].calendar)
                 dt_idx = [tt for tt in range(len(mask_times)) if dt_this == mask_times[tt]]
             except:
@@ -730,16 +730,8 @@ def calc_composite_lpt_mask(dt_begin, dt_end, interval_hours, prod='trmm'
         dt00 = dt_begin - dt.timedelta(hours=accumulation_hours) #(dt_begin - dt.datetime(1970,1,1,0,0,0)).total_seconds()/3600.0 - accumulation_hours
     else:
         dt00 = dt_begin #(dt_begin - dt.datetime(1970,1,1,0,0,0)).total_seconds()/3600.0
-    #grand_mask_timestamps1 = (dt_end - dt.datetime(1970,1,1,0,0,0)).total_seconds()/3600.0
-
-    #grand_mask_timestamps = np.arange(grand_mask_timestamps0, grand_mask_timestamps1 + dt_hours, dt_hours)
     grand_mask_times = lpt.helpers.dtrange(dt00, dt_end + dt.timedelta(hours=int(dt_hours)), dt_hours)   # [dt.datetime(1970,1,1,0,0,0) + dt.timedelta(hours=x) for x in grand_mask_timestamps]
-    print(grand_mask_times)
 
-
-
-    ## Get grid info. This is needed in case there are no MJO LPTs and you are doing a
-    ##   composite mask with MJO LPTs only, since otherwise it would not go in to the loop.
 
     ## Initialize the mask arrays dictionary if this is the first LP object.
     ## First, I need the grid information. Get this from the first LP object.
@@ -802,18 +794,19 @@ def calc_composite_lpt_mask(dt_begin, dt_end, interval_hours, prod='trmm'
         this_lpt_idx = np.argwhere(TC['lptid'] == this_lpt_id)[0][0]
         lp_object_id_list = TC['objid'][this_lpt_idx,0:int(TC['num_objects'][this_lpt_idx])]
 
-        dt0 = dt.datetime.strptime(str(int(np.min(lp_object_id_list)))[0:10],'%Y%m%d%H') - dt.timedelta(hours=accumulation_hours)
-        dt1 = dt.datetime.strptime(str(int(np.max(lp_object_id_list)))[0:10],'%Y%m%d%H')
+        dt00 = dt.datetime.strptime(str(int(np.min(lp_object_id_list))).zfill(14)[0:10],'%Y%m%d%H')
+        dt0 = cftime.datetime(dt00.year,dt00.month,dt00.day,dt00.hour,calendar=TC['datetime'][0].calendar)
+        dt11 = dt.datetime.strptime(str(int(np.max(lp_object_id_list))).zfill(14)[0:10],'%Y%m%d%H')
+        dt1 = cftime.datetime(dt11.year,dt11.month,dt11.day,dt11.hour,calendar=TC['datetime'][0].calendar)
         duration_hours = int((dt1 - dt0).total_seconds()/3600)
-        mask_times = [dt0 + dt.timedelta(hours=x) for x in range(0,duration_hours+1,interval_hours)]
 
         for lp_object_id in lp_object_id_list:
 
             nnnn = int(str(int(lp_object_id))[-4:])
             try:
-                dt_this = lpt.helpers.get_objid_datetime(lp_object_id) # dt.datetime.strptime(str(int(lp_object_id))[0:10],'%Y%m%d%H')
-                timestamp_this = (dt_this - dt.datetime(1970,1,1,0,0,0)).total_seconds()/3600.0
-                dt_idx = [tt for tt in range(len(grand_mask_timestamps)) if timestamp_this == grand_mask_timestamps[tt]]
+                dt_this0 = lpt.helpers.get_objid_datetime(lp_object_id)
+                dt_this = cftime.datetime(dt_this0.year,dt_this0.month,dt_this0.day,dt_this0.hour,calendar=TC['datetime'][0].calendar)
+                dt_idx = [tt for tt in range(len(grand_mask_times)) if dt_this == grand_mask_times[tt]]
             except:
                 continue
 
@@ -835,7 +828,6 @@ def calc_composite_lpt_mask(dt_begin, dt_end, interval_hours, prod='trmm'
             try:
                 iii = DS['pixels_x'][nnnn,:].compressed()
                 jjj = DS['pixels_y'][nnnn,:].compressed()
-
             except:
                 DS.close()
                 continue
@@ -893,46 +885,75 @@ def calc_composite_lpt_mask(dt_begin, dt_end, interval_hours, prod='trmm'
     else:
         fn_out = (mask_output_dir+'/lpt_composite_mask_'+YMDH1_YMDH2+'.nc')
 
-    os.remove(fn_out) if os.path.exists(fn_out) else None
-    print('Writing to: ' + fn_out, flush=True)
-    DSnew = Dataset(fn_out, 'w', data_model='NETCDF4', clobber=True)
-    DSnew.createDimension('n', 1)
-    DSnew.createDimension('time',len(grand_mask_timestamps))
-    DSnew.createDimension('lon',len(grand_mask_lon))
-    DSnew.createDimension('lat',len(grand_mask_lat))
-    DSnew.createVariable('lon','f4',('lon',))
-    DSnew.createVariable('lat','f4',('lat',))
-    DSnew.createVariable('grid_area','f4',('lat','lon'))
-    DSnew.createVariable('time','d',('time',))
 
-    DSnew['time'][:] = grand_mask_timestamps
-    DSnew['time'].setncattr('units','hours since 1970-1-1 0:0:0')
-    DSnew['lon'][:] = grand_mask_lon
-    DSnew['lon'].setncattr('units','degrees_east')
-    DSnew['lat'][:] = grand_mask_lat
-    DSnew['lat'].setncattr('units','degrees_north')
+    ## Set coordinates
+    coords_dict = {}
+    coords_dict['n'] = (['n',], [1,])
+    coords_dict['time'] = (['time',], grand_mask_times)
+    coords_dict['lon'] = (['lon',], grand_mask_lon, {'units':'degrees_east'})
+    coords_dict['lat'] = (['lat',], grand_mask_lat, {'units':'degrees_north'})
 
-    DSnew['grid_area'][:] = AREA
-    DSnew['grid_area'].setncattr('units','km2')
-    DSnew['grid_area'].setncattr('description','Area of each grid cell.')
+    ## Set Data
+    data_dict = {}
+    data_dict['grid_area'] = (['lat','lon',], AREA, {'units':'km2','description':'Area of each grid cell.'})
 
-    ## Write volrain if it was calculated.
+    ## Basic LPT system track data for convenience
+    # for mask_var in basic_lpt_info_field_list:
+    #     data_dict[mask_var] = (['time',], mask_arrays[mask_var])
+
+    # Volumetric Rain, if specified.
     if do_volrain:
         fields = [*VOLRAIN]
-        fields = [x for x in fields if not 'tser' in x]
         for field in fields:
-            add_volrain_to_netcdf(DSnew, field, VOLRAIN[field]
-                    , field+'_tser', VOLRAIN[field+'_tser']
-                    , fill_value = FILL_VALUE)
+            if 'tser' in field:
+                data_dict[field] = (['time',], VOLRAIN[field])
+            else:
+                data_dict[field] = (['n',], [VOLRAIN[field],])
+
+    # for mask_var in ['duration','maxarea','zonal_propagation_speed','meridional_propagation_speed']:
+    #     print(mask_var, mask_arrays[mask_var])
+    #     data_dict[mask_var] = (['n',], [mask_arrays[mask_var],])
+
+
+    ## Create XArray Dataset
+    DS = xr.Dataset(data_vars=data_dict, coords=coords_dict)
+    # DS.centroid_lon.attrs = {'units':'degrees_east','long_name':'centroid longitude (0-360)','standard_name':'longitude','note':'Time is end of running mean time.'}
+    # DS.centroid_lat.attrs = {'units':'degrees_north','long_name':'centroid latitude (-90-00)','standard_name':'latitude','note':'Time is end of running mean time.'}
+    # DS.largest_object_centroid_lon.attrs = {'units':'degrees_east','long_name':'centroid longitude (0-360)','standard_name':'longitude','note':'Time is end of running mean time.'}
+    # DS.largest_object_centroid_lat.attrs = {'units':'degrees_east','long_name':'centroid latitude (-90-00)','standard_name':'latitude','note':'Time is end of running mean time.'}
+    # DS.area.attrs = {'units':'km2','long_name':'LPT System enclosed area','note':'Time is end of running mean time.'}
+
+    # Time varying fields
+    # for var in ['max_filtered_running_field','max_running_field','max_inst_field'
+    #             ,'min_filtered_running_field','min_running_field','min_inst_field'
+    #             ,'amean_filtered_running_field','amean_running_field','amean_inst_field']:
+    #     DS[var].attrs = {'units':'mm day-1',
+    #             'long_name':'LP object running mean rain rate (at end of accum time).',
+    #             'note':'Time is end of running mean time. Based on mask_at_end_time'}
+
+    # DS.maxarea.attrs = {'units':'km2','long_name':'LPT System enclosed area','note':'This is the max over the LPT life time.'}
+    # DS.duration.attrs = {'units':'h','long_name':'LPT System duration'}
+    # DS.zonal_propagation_speed.attrs = {'units':'m s-1','long_name':'Zonal popagation speed','description':'Zonal popagation speed of the entire LPT system -- based on least squares fit of lon(time).'}
+    # DS.meridional_propagation_speed.attrs = {'units':'m s-1','long_name':'meridional popagation speed','description':'Meridional popagation speed of the entire LPT system -- based on least squares fit of lon(time).'}
+
+
+
+    ## Write the data to NetCDF
+    # fn_out = (mask_output_dir + '/' + YMDH1_YMDH2 + '/lpt_system_mask_'+prod+'.lptid{0:010.4f}.nc'.format(this_lpt_id))
+    os.makedirs(mask_output_dir, exist_ok=True)
+
+    print('Writing to: ' + fn_out, flush=True)
+    DS.to_netcdf(path=fn_out, mode='w', unlimited_dims=['time',], encoding={'time': {'dtype': 'i'}, 'n': {'dtype': 'i'}})
+    
 
     ## Define the mask variables here.
     ## But don't assign them yet.
-    fields = [*mask_arrays]
-    fields = [x for x in fields if 'mask' in x]
-    for field in fields:
-        DSnew.createVariable(field,'i1',('time','lat','lon'),zlib=True,complevel=4)
-        DSnew[field].setncattr('units','1')
-    DSnew.close()
+    with Dataset(fn_out, 'a') as DSnew:
+        fields = [*mask_arrays]
+        fields = [x for x in fields if 'mask' in x]
+        for field in fields:
+            DSnew.createVariable(field,'i1',('time','lat','lon'),zlib=True,complevel=4)
+            DSnew[field].setncattr('units','1')
 
     ## Writing mask variables.
     ## Do them one at a time so it uses less memory while writing them.
@@ -940,3 +961,52 @@ def calc_composite_lpt_mask(dt_begin, dt_end, interval_hours, prod='trmm'
         print('- ' + field)
         add_mask_var_to_netcdf(fn_out, field, mask_arrays[field]
                             , memory_target_mb = memory_target_mb)
+
+
+    # os.remove(fn_out) if os.path.exists(fn_out) else None
+    # print('Writing to: ' + fn_out, flush=True)
+    # DSnew = Dataset(fn_out, 'w', data_model='NETCDF4', clobber=True)
+    # DSnew.createDimension('n', 1)
+    # DSnew.createDimension('time',len(grand_mask_timestamps))
+    # DSnew.createDimension('lon',len(grand_mask_lon))
+    # DSnew.createDimension('lat',len(grand_mask_lat))
+    # DSnew.createVariable('lon','f4',('lon',))
+    # DSnew.createVariable('lat','f4',('lat',))
+    # DSnew.createVariable('grid_area','f4',('lat','lon'))
+    # DSnew.createVariable('time','d',('time',))
+    #
+    # DSnew['time'][:] = grand_mask_timestamps
+    # DSnew['time'].setncattr('units','hours since 1970-1-1 0:0:0')
+    # DSnew['lon'][:] = grand_mask_lon
+    # DSnew['lon'].setncattr('units','degrees_east')
+    # DSnew['lat'][:] = grand_mask_lat
+    # DSnew['lat'].setncattr('units','degrees_north')
+    #
+    # DSnew['grid_area'][:] = AREA
+    # DSnew['grid_area'].setncattr('units','km2')
+    # DSnew['grid_area'].setncattr('description','Area of each grid cell.')
+    #
+    # ## Write volrain if it was calculated.
+    # if do_volrain:
+    #     fields = [*VOLRAIN]
+    #     fields = [x for x in fields if not 'tser' in x]
+    #     for field in fields:
+    #         add_volrain_to_netcdf(DSnew, field, VOLRAIN[field]
+    #                 , field+'_tser', VOLRAIN[field+'_tser']
+    #                 , fill_value = FILL_VALUE)
+    #
+    # ## Define the mask variables here.
+    # ## But don't assign them yet.
+    # fields = [*mask_arrays]
+    # fields = [x for x in fields if 'mask' in x]
+    # for field in fields:
+    #     DSnew.createVariable(field,'i1',('time','lat','lon'),zlib=True,complevel=4)
+    #     DSnew[field].setncattr('units','1')
+    # DSnew.close()
+    #
+    # ## Writing mask variables.
+    # ## Do them one at a time so it uses less memory while writing them.
+    # for field in fields:
+    #     print('- ' + field)
+    #     add_mask_var_to_netcdf(fn_out, field, mask_arrays[field]
+    #                         , memory_target_mb = memory_target_mb)
