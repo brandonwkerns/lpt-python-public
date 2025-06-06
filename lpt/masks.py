c19f2ffc698dd7e316711b067b8eda5378c3a06a
brandonwkerns <bkerns@uw.edu>
2025-06-06 16:51:16 -0700

    Move helper type functions to the top

--- lpt/masks.py
+++ lpt/masks.py
@@ -468,194 +468,6 @@ def add_volrain_to_netcdf(DS, volrain_var_sum, data_sum
     DS[volrain_var_tser].setncatts({'units':'mm - km2','description':'Time Series of Volumetric Rain','description':'Volumetric rain (sum of area * raw rain rate).'})
 
 
-################################################################################
-################################################################################
-################################################################################
-###########  calc_lpo_mask #####################################################
-################################################################################
-################################################################################
-################################################################################
-
-def calc_lpo_mask(dt_begin, dt_end, interval_hours, accumulation_hours = 0, filter_stdev = 0
-    , lp_objects_dir = '.', lp_objects_fn_format='objects_%Y%m%d%H.nc', mask_output_dir = '.'
-    , detailed_output = False
-    , include_rain_rates=False, do_volrain=False, dataset_dict = {}
-    , calc_with_filter_radius = True
-    , calc_with_accumulation_period = True
-    , cold_start_mode = False
-    , coarse_grid_factor = 0
-    , multiply_factor = 1.0
-    , units = '1'
-    , memory_target_mb = 1000
-    , nproc = 1):
-
-    MISSING = -999.0
-    FILL_VALUE = MISSING
-
-    """
-    dt_begin, dt_end: datetime objects for the first and last times. These are END of accumulation times!
-    """
-
-    YMDH1_YMDH2 = (dt_begin.strftime('%Y%m%d%H') + '_' + dt_end.strftime('%Y%m%d%H'))
-
-    dt1 = dt_end
-    if accumulation_hours > 0 and calc_with_accumulation_period and not cold_start_mode: #Cold start mode doesn't have data before init time.
-        dt0 = dt_begin - dt.timedelta(hours=accumulation_hours)
-        dt_idx0 = int(accumulation_hours/interval_hours)
-    else:
-        dt0 = dt_begin
-        dt_idx0 = 0
-    duration_hours = int((dt1 - dt0).total_seconds()/3600)
-    mask_times = [dt0 + dt.timedelta(hours=x) for x in range(0,duration_hours+interval_hours,interval_hours)]
-    mask_arrays = None
-
-    # Collect list of LP objects.
-    lp_object_id_list = []
-
-    for dt_idx in range(dt_idx0, len(mask_times)):
-        this_dt = mask_times[dt_idx]
-
-        if dt_idx == dt_idx0 or dt_idx == len(mask_times)-1 or np.mod(dt_idx, 100) == 0:
-            print(('Get LPO IDs: ' + this_dt.strftime('%Y%m%d%H') + ' ('+str(dt_idx)+' of '+str(len(mask_times)-1)+')'), flush=True)
-
-        fn = (lp_objects_dir + '/' + this_dt.strftime(lp_objects_fn_format)).replace('///','/').replace('//','/')
-        with Dataset(fn) as DS:
-            if 'objid' in DS.variables:
-                this_lp_object_id_list = DS['objid'][:].compressed().tolist()
-                lp_object_id_list.extend(this_lp_object_id_list)
-            else:
-                print('WARNING: For LPO mask calculation, Objects file does not have "object_id" variable: ' + fn)
-                continue
-
-    # Initialize masks.
-    fn = (lp_objects_dir + '/' + mask_times[0].strftime(lp_objects_fn_format)).replace('///','/').replace('//','/')
-    with Dataset(fn) as ds:
-        mask_lon = ds['grid_lon'][:]
-        mask_lat = ds['grid_lat'][:]
-        AREA = ds['grid_area'][:]
-
-    print('Initialize Mask:')
-    print('Mask times: {}'.format(len(mask_times)))
-    print('Mask lon/lat shape: {}, {}'.format(mask_lon.shape, mask_lat.shape))
-    mask_arrays = initialize_mask_arrays(
-        len(mask_lat), len(mask_lon), len(mask_times),
-        detailed_output,
-        calc_with_accumulation_period, accumulation_hours,
-        calc_with_filter_radius)
-
-    # Fill in the mask array data for the list of LP objects.
-    print('Fill in the mask arrays for {} LP objects.'.format(
-        len(lp_object_id_list)))
-    mask_arrays = fill_mask_arrays(
-        lp_object_id_list, mask_times, mask_arrays,
-        lp_objects_dir, lp_objects_fn_format,
-        detailed_output, accumulation_hours, interval_hours,
-        calc_with_accumulation_period, filter_stdev,
-        coarse_grid_factor, nproc=nproc
-    )
-
-    # Do filter width spreading if specified.
-    do_filter = determine_filtering(filter_stdev, calc_with_filter_radius)
-
-    if do_filter:
-        print('Filter width spreading...', flush=True)
-
-        # Do the filter width spreading.
-        mask_arrays = add_filter_width_spreading(
-            mask_arrays, filter_stdev, detailed_output,
-            calc_with_accumulation_period, accumulation_hours,
-            coarse_grid_factor, nproc=nproc
-        )
-
-    ## Do volumetric rain.
-    if do_volrain:
-        print('Now calculating the volumetric rain.', flush=True)
-        VOLRAIN = mask_calc_volrain(mask_times,interval_hours,multiply_factor,AREA,mask_arrays,dataset_dict,nproc=nproc)
-
-    ##
-    ## Output.
-    ##
-
-    ## Set coordinates
-    coords_dict = {}
-    coords_dict['n'] = (['n',], [1,])
-    coords_dict['time'] = (['time',], mask_times)
-    coords_dict['lon'] = (['lon',], mask_lon, {'units':'degrees_east'})
-    coords_dict['lat'] = (['lat',], mask_lat, {'units':'degrees_north'})
-
-    ## Set Data
-    data_dict = {}
-    data_dict['grid_area'] = (['lat','lon',], AREA, {'units':'km2','description':'Area of each grid cell.'})
-
-    # Volumetric Rain, if specified.
-    if do_volrain:
-        fields = [*VOLRAIN]
-        for field in fields:
-            if 'tser' in field:
-                data_dict[field] = (['time',], VOLRAIN[field])
-            else:
-                data_dict[field] = (['n',], [VOLRAIN[field],])
-    
-    ## Create XArray Dataset
-    DS = xr.Dataset(data_vars=data_dict, coords=coords_dict)
-
-    ## Write the data to NetCDF
-    fn_out = (mask_output_dir + '/lp_objects_mask_' + YMDH1_YMDH2 + '.nc').replace('///','/').replace('//','/')
-    os.makedirs(mask_output_dir, exist_ok=True)
-
-    print('Writing to: ' + fn_out, flush=True)
-    DS.to_netcdf(path=fn_out, mode='w', unlimited_dims=['time',], encoding={'time': {'dtype': 'i'}, 'n': {'dtype': 'i'}})
-
-    ## Define the mask variables here.
-    ## But don't assign them yet.
-    DSnew = Dataset(fn_out, 'a')
-    fields = [*mask_arrays]
-    fields = [x for x in fields if 'mask' in x]
-    for field in fields:
-        dtype = 'i1'
-        this_units = '1'
-        DSnew.createVariable(field,dtype,('time','lat','lon'),zlib=True,complevel=4)
-        DSnew[field].setncattr('units',this_units)
-    DSnew.close()
-
-    ## Writing mask variables.
-    ## Do them one at a time so it uses less memory while writing them.
-    for field in fields:
-        print('- ' + field)
-        add_mask_var_to_netcdf(fn_out, field, mask_arrays[field]
-                            , memory_target_mb = memory_target_mb)
-
-    ## Include masked rain rates, if specified.
-    if include_rain_rates:
-        print('Adding masked rainfall.', flush=True)
-        fields = [*mask_arrays]
-        fields = [x for x in fields if 'mask' in x]
-        dtype = 'float32'
-        this_units = units
-
-        for field in fields:
-            new_field = field + '_with_rain'
-            print(new_field)
-            mask_array_with_rain = add_masked_rain_rates(mask_arrays[field], mask_times, multiply_factor, dataset_dict, nproc=nproc)
-
-            with Dataset(fn_out, 'a') as DSnew:
-                DSnew.createVariable(new_field,dtype,('time','lat','lon'),zlib=True,complevel=4)
-                DSnew[new_field].setncattr('units',this_units)
-            add_mask_var_to_netcdf(fn_out, new_field, mask_array_with_rain
-                                , memory_target_mb = memory_target_mb, data_mask = mask_arrays[field])
-
-        print('Done adding masked rainfall.')
-
-
-################################################################################
-################################################################################
-################################################################################
-###########  calc_individual_lpt_masks #########################################
-################################################################################
-################################################################################
-################################################################################
-
-
 def get_lpo_grid_coords(lp_object_id, lp_objects_dir, lp_objects_fn_format):
 
     dt_this0 = lpt.helpers.get_objid_datetime(lp_object_id)
@@ -897,6 +709,194 @@ def add_filter_width_spreading(
         )
 
     return mask_arrays
+
+
+################################################################################
+################################################################################
+################################################################################
+###########  calc_lpo_mask #####################################################
+################################################################################
+################################################################################
+################################################################################
+
+def calc_lpo_mask(dt_begin, dt_end, interval_hours, accumulation_hours = 0, filter_stdev = 0
+    , lp_objects_dir = '.', lp_objects_fn_format='objects_%Y%m%d%H.nc', mask_output_dir = '.'
+    , detailed_output = False
+    , include_rain_rates=False, do_volrain=False, dataset_dict = {}
+    , calc_with_filter_radius = True
+    , calc_with_accumulation_period = True
+    , cold_start_mode = False
+    , coarse_grid_factor = 0
+    , multiply_factor = 1.0
+    , units = '1'
+    , memory_target_mb = 1000
+    , nproc = 1):
+
+    MISSING = -999.0
+    FILL_VALUE = MISSING
+
+    """
+    dt_begin, dt_end: datetime objects for the first and last times. These are END of accumulation times!
+    """
+
+    YMDH1_YMDH2 = (dt_begin.strftime('%Y%m%d%H') + '_' + dt_end.strftime('%Y%m%d%H'))
+
+    dt1 = dt_end
+    if accumulation_hours > 0 and calc_with_accumulation_period and not cold_start_mode: #Cold start mode doesn't have data before init time.
+        dt0 = dt_begin - dt.timedelta(hours=accumulation_hours)
+        dt_idx0 = int(accumulation_hours/interval_hours)
+    else:
+        dt0 = dt_begin
+        dt_idx0 = 0
+    duration_hours = int((dt1 - dt0).total_seconds()/3600)
+    mask_times = [dt0 + dt.timedelta(hours=x) for x in range(0,duration_hours+interval_hours,interval_hours)]
+    mask_arrays = None
+
+    # Collect list of LP objects.
+    lp_object_id_list = []
+
+    for dt_idx in range(dt_idx0, len(mask_times)):
+        this_dt = mask_times[dt_idx]
+
+        if dt_idx == dt_idx0 or dt_idx == len(mask_times)-1 or np.mod(dt_idx, 100) == 0:
+            print(('Get LPO IDs: ' + this_dt.strftime('%Y%m%d%H') + ' ('+str(dt_idx)+' of '+str(len(mask_times)-1)+')'), flush=True)
+
+        fn = (lp_objects_dir + '/' + this_dt.strftime(lp_objects_fn_format)).replace('///','/').replace('//','/')
+        with Dataset(fn) as DS:
+            if 'objid' in DS.variables:
+                this_lp_object_id_list = DS['objid'][:].compressed().tolist()
+                lp_object_id_list.extend(this_lp_object_id_list)
+            else:
+                print('WARNING: For LPO mask calculation, Objects file does not have "object_id" variable: ' + fn)
+                continue
+
+    # Initialize masks.
+    fn = (lp_objects_dir + '/' + mask_times[0].strftime(lp_objects_fn_format)).replace('///','/').replace('//','/')
+    with Dataset(fn) as ds:
+        mask_lon = ds['grid_lon'][:]
+        mask_lat = ds['grid_lat'][:]
+        AREA = ds['grid_area'][:]
+
+    print('Initialize Mask:')
+    print('Mask times: {}'.format(len(mask_times)))
+    print('Mask lon/lat shape: {}, {}'.format(mask_lon.shape, mask_lat.shape))
+    mask_arrays = initialize_mask_arrays(
+        len(mask_lat), len(mask_lon), len(mask_times),
+        detailed_output,
+        calc_with_accumulation_period, accumulation_hours,
+        calc_with_filter_radius)
+
+    # Fill in the mask array data for the list of LP objects.
+    print('Fill in the mask arrays for {} LP objects.'.format(
+        len(lp_object_id_list)))
+    mask_arrays = fill_mask_arrays(
+        lp_object_id_list, mask_times, mask_arrays,
+        lp_objects_dir, lp_objects_fn_format,
+        detailed_output, accumulation_hours, interval_hours,
+        calc_with_accumulation_period, filter_stdev,
+        coarse_grid_factor, nproc=nproc
+    )
+
+    # Do filter width spreading if specified.
+    do_filter = determine_filtering(filter_stdev, calc_with_filter_radius)
+
+    if do_filter:
+        print('Filter width spreading...', flush=True)
+
+        # Do the filter width spreading.
+        mask_arrays = add_filter_width_spreading(
+            mask_arrays, filter_stdev, detailed_output,
+            calc_with_accumulation_period, accumulation_hours,
+            coarse_grid_factor, nproc=nproc
+        )
+
+    ## Do volumetric rain.
+    if do_volrain:
+        print('Now calculating the volumetric rain.', flush=True)
+        VOLRAIN = mask_calc_volrain(mask_times,interval_hours,multiply_factor,AREA,mask_arrays,dataset_dict,nproc=nproc)
+
+    ##
+    ## Output.
+    ##
+
+    ## Set coordinates
+    coords_dict = {}
+    coords_dict['n'] = (['n',], [1,])
+    coords_dict['time'] = (['time',], mask_times)
+    coords_dict['lon'] = (['lon',], mask_lon, {'units':'degrees_east'})
+    coords_dict['lat'] = (['lat',], mask_lat, {'units':'degrees_north'})
+
+    ## Set Data
+    data_dict = {}
+    data_dict['grid_area'] = (['lat','lon',], AREA, {'units':'km2','description':'Area of each grid cell.'})
+
+    # Volumetric Rain, if specified.
+    if do_volrain:
+        fields = [*VOLRAIN]
+        for field in fields:
+            if 'tser' in field:
+                data_dict[field] = (['time',], VOLRAIN[field])
+            else:
+                data_dict[field] = (['n',], [VOLRAIN[field],])
+    
+    ## Create XArray Dataset
+    DS = xr.Dataset(data_vars=data_dict, coords=coords_dict)
+
+    ## Write the data to NetCDF
+    fn_out = (mask_output_dir + '/lp_objects_mask_' + YMDH1_YMDH2 + '.nc').replace('///','/').replace('//','/')
+    os.makedirs(mask_output_dir, exist_ok=True)
+
+    print('Writing to: ' + fn_out, flush=True)
+    DS.to_netcdf(path=fn_out, mode='w', unlimited_dims=['time',], encoding={'time': {'dtype': 'i'}, 'n': {'dtype': 'i'}})
+
+    ## Define the mask variables here.
+    ## But don't assign them yet.
+    DSnew = Dataset(fn_out, 'a')
+    fields = [*mask_arrays]
+    fields = [x for x in fields if 'mask' in x]
+    for field in fields:
+        dtype = 'i1'
+        this_units = '1'
+        DSnew.createVariable(field,dtype,('time','lat','lon'),zlib=True,complevel=4)
+        DSnew[field].setncattr('units',this_units)
+    DSnew.close()
+
+    ## Writing mask variables.
+    ## Do them one at a time so it uses less memory while writing them.
+    for field in fields:
+        print('- ' + field)
+        add_mask_var_to_netcdf(fn_out, field, mask_arrays[field]
+                            , memory_target_mb = memory_target_mb)
+
+    ## Include masked rain rates, if specified.
+    if include_rain_rates:
+        print('Adding masked rainfall.', flush=True)
+        fields = [*mask_arrays]
+        fields = [x for x in fields if 'mask' in x]
+        dtype = 'float32'
+        this_units = units
+
+        for field in fields:
+            new_field = field + '_with_rain'
+            print(new_field)
+            mask_array_with_rain = add_masked_rain_rates(mask_arrays[field], mask_times, multiply_factor, dataset_dict, nproc=nproc)
+
+            with Dataset(fn_out, 'a') as DSnew:
+                DSnew.createVariable(new_field,dtype,('time','lat','lon'),zlib=True,complevel=4)
+                DSnew[new_field].setncattr('units',this_units)
+            add_mask_var_to_netcdf(fn_out, new_field, mask_array_with_rain
+                                , memory_target_mb = memory_target_mb, data_mask = mask_arrays[field])
+
+        print('Done adding masked rainfall.')
+
+
+################################################################################
+################################################################################
+################################################################################
+###########  calc_individual_lpt_masks #########################################
+################################################################################
+################################################################################
+################################################################################
 
 
 def calc_individual_lpt_masks(dt_begin, dt_end, interval_hours, prod='trmm'
